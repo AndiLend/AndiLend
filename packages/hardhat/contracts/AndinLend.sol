97071ff36aefbb8b1fe4b52e5a05bf4a8dc53fe7
Rodrigo Goitia <rodrigojgq@gmail.com>
2024-03-23 20:23:02 -0300

    AndinLend contract first version

--- packages/hardhat/contracts/AndinLend.sol
+++ packages/hardhat/contracts/AndinLend.sol
@@ -3,6 +3,10 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+/**
+ * A smart contract that allows users to create loans requests and lenders to borrow USDTs
+ * Also returns the lender the borrowed money.
+ */
 contract AndinLend {
 	struct Loan {
 		uint amount;
@@ -19,6 +23,29 @@ contract AndinLend {
 	mapping(address => Loan) public loans;
 	mapping(address => address) private borrowerToLender;
 	mapping(address => address[]) private lenderToBorrowers;
+
+	event RequestedLoan(
+		address borrower,
+		Loan loan,
+	);
+
+	event GrantedLoan(
+		address borrower,
+		address lender,
+		Loan loan,
+	);
+
+	event PaidFee(
+		address borrower,
+		address lender,
+		Loan loan,
+	);
+
+	event FinishLoan(
+		address borrower,
+		address lender,
+		Loan loan,
+	);
 
 	IERC20 private erc20USDT;
 	// Verifier private verifier
@@ -50,6 +77,7 @@ contract AndinLend {
 			_proof
 		);
 		loans[msg.sender] = newLoanRequest;
+		emit(RequestedLoan(msg.sender, newLoanRequest));
 	}
 
 	function grantLoan(address memory _borrower) public {
@@ -58,31 +86,35 @@ contract AndinLend {
 			"The client does not have a loan requirement"
 		);
 		require(loans[_borrower].status == 0, "The loan is not pending");
+		require(erc20USDT.balanceOf(msg.sender) > loans[_borrower].amount, 'Insufficient USDT balance.');
 
-		// To Do verify how to transfer from one to another.
 		erc20USDT.transferFrom(msg.sender, _borrower, loans[_borrower].amount);
 		loans[_borrower].status = 1;
 		borrowerToLender[_borrower] = msg.sender;
 		lenderToBorrowers[msg.sender].push(_borrower);
+		emit GrantedLoan(_borrower, msg.sender, loans[_borrower]);
 	}
 
   function payFee(address _lender) external {
-    require(loans[msg.sender], 'Client does not have an active loan');
-    require(loans[msg.sender].status == 1, 'The loan is not active');
-    // To Do review quantity
+    require(loans[msg.sender], 'Client does not have an active loan.');
+    require(loans[msg.sender].status == 1, 'The loan is not active.');
+		require(erc20USDT.balanceOf(msg.sender) > loans[msg.sender].fee, 'Insufficient USDT balance.');
+		require(borrowerToLender[msg.sender] == _lender, "The address is not the lender of this loan.")
 
     erc20USDT.transferFrom(msg.sender, _lender, loans[msg.sender].fee);
     loans[msg.sender].pendingFeesAmount = sub(loans[msg.sender].pendingFeesAmount, 1);
+		emit PaidFee( msg.sender, _lender, loans[msg.sender]);
     if(loans[msg.sender].pendingFeesAmount == 0){
-      finishLoan();
+      finishLoan(_lender);
     }
   }
 
-  function finishLoan() private {
+  function finishLoan(address _lender) private {
     loans[msg.sender].status = 2;
+		emit FinishLoan( msg.sender, _lender, loans[msg.sender]);
   }
 
-	function getLoanByAddress(address _borrower) public view returns(Loan){
+	function getLoanByAddress(address _borrower) external view returns(Loan){
 		return loans[_borrower];
 	}
 
