7fbdd1ad2485610d99b87975756dea3c2df3e5c4
esteblock <estebaniglesiasmanriquez@gmail.com>
2024-03-23 18:58:22 -0300

    zero toml

--- packages/zk_credit_score/test/index.js
+++ packages/zk_credit_score/test/index.js
@@ -20,8 +20,18 @@ value = "${paddedLeafRight}"
     return tomlContent;
 }
 
+function generateZeroProverToml(){
+    const address ="0x0000000000000000000000000000000000000000";
+    const creditScore = 0;
+    return generateProverToml(address, creditScore)
+}
+
 const testAddress = "0x7a9f3B6e44C2e808d3Ff7a4e3e15C6c5e8D42ABC";
 const testCreditScore = 800;
 
 const proverToml = generateProverToml(testAddress, testCreditScore);
 console.log(proverToml);
+
+const zeroProverToml = generateZeroProverToml();
+console.log("🚀 ~ zeroProverToml:", zeroProverToml)
+
