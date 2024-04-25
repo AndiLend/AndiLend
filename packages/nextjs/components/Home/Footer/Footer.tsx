0a27006a2605ac2f202f1b6dee5122563673587c
Alejandro Alvarez <a.alejandro.alvarez.a@gmail.com>
2024-04-25 15:56:28 -0400

    style(footer): style footer section

--- packages/nextjs/components/Home/Footer/Footer.tsx
+++ packages/nextjs/components/Home/Footer/Footer.tsx
@@ -5,20 +5,22 @@ const Footer = () => {
   return (
     <div className="bg-primary">
       <div className="max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300 ">
-        <div>
-          <span className="w-full text-3xl font-bold">Crypto</span>
-          <span className="w-full text-3xl font-bold text-[#00df9a]">Cap.</span>
-          <p className="py-4"></p>
-          <div className="flex justify-between md:w-[75%] my-6">
-            <FaFacebookSquare size={30} />
-            <FaInstagram size={30} />
-            <FaTwitterSquare size={30} />
-            <FaGithubSquare size={30} />
+        <div className="flex flex-col justify-between items-baseline h-full">
+          <span className="w-full text-3xl font-bold text-white">AndiLending</span>
+          <div className="flex flex-col gap-2">
+            <div className="flex justify-between md:w-[75%]">
+              <FaFacebookSquare size={30} />
+              <FaInstagram size={30} />
+              <FaTwitterSquare size={30} />
+              <FaGithubSquare size={30} />
+            </div>
+            <div>2024 AndiLending. All rights reserved</div>
           </div>
         </div>
-        <div className="lg:col-span-2 flex justify-between mt-6">
+
+        <div className="lg:col-span-2 flex justify-around h-full">
           <div>
-            <h6 className="font-medium text-gray-400">About us</h6>
+            <h6 className="font-medium text-white">About us</h6>
             <ul>
               <li className="py-2 text-sm">About</li>
               <li className="py-2 text-sm">Careers</li>
@@ -26,17 +28,9 @@ const Footer = () => {
               <li className="py-2 text-sm">Legal & Privacy</li>
             </ul>
           </div>
+
           <div>
-            <h6 className="font-medium text-gray-400">Services</h6>
-            <ul>
-              <li className="py-2 text-sm">Applications</li>
-              <li className="py-2 text-sm">Buy Crypto</li>
-              <li className="py-2 text-sm">Affiliate</li>
-              <li className="py-2 text-sm">Institutional Services</li>
-            </ul>
-          </div>
-          <div>
-            <h6 className="font-medium text-gray-400">Learn</h6>
+            <h6 className="font-medium text-white">Learn</h6>
             <ul>
               <li className="py-2 text-sm">What is Cryptocurrency?</li>
               <li className="py-2 text-sm">Crypto Basic</li>
